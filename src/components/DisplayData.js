5f4bd43a29e497b182d9d273a23b13a782bf26f3
unknown <boni.leonardor@outlook.com>
2025-04-06 17:31:21 -0300

    Enviando primeiro commit do projeto form react

---
+++ src/components/DisplayData.js
@@ -0,0 +1,18 @@
+import React from "react";
+import '../index.css'; 
+
+const DisplayData = ({ data }) => {
+  return (
+    <div>
+      <h2>Dados Submetidos:</h2>
+      <p>Nome: {data.name}</p>
+      <p>Email: {data.email}</p>
+      <p>Cidade: {data.city}</p>
+      <p>Trabalho: {data.job}</p>
+      <p>Data de nascimento: {data.birth}</p>
+      <p>Senha: {data.password}</p>
+    </div>
+  );
+};
+
+export default DisplayData;
---
+++ src/components/Form.js
@@ -0,0 +1,145 @@
+import React, { Component } from "react";
+import DisplayData from "./DisplayData"; 
+import '../index.css'; 
+
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      email: "",
+      city: "",
+      job: "",
+      birth: "",
+      password: "",
+      submittedData: [], 
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+   
+    this.setState((prevState) => ({
+      submittedData: [
+        ...prevState.submittedData,
+        {
+          name: prevState.name,
+          email: prevState.email,
+          city: prevState.city,
+          job: prevState.job,
+          birth: prevState.birth,
+          password: prevState.password
+        },
+      ],
+      name: "",
+      email: "",
+      city: "",
+      job: "",
+      birth: "",
+      password: "",
+    }));
+  };
+
+  render() {
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <div>
+            <label>
+              Nome:
+              <input
+                type="text"
+                name="name"
+                value={this.state.name}
+                onChange={this.handleChange}
+                required
+              />
+            </label>
+          </div>
+          <div>
+            <label>
+              Email:
+              <input
+                type="email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+                required
+              />
+            </label>
+          </div>
+
+          <div>
+            <label>
+              Cidade:
+              <input
+                type="text"
+                name="city"
+                value={this.state.city}
+                onChange={this.handleChange}
+                required
+              />
+            </label>
+          </div>
+
+          <div>
+            <label>
+              Trabalho:
+              <input
+                type="text"
+                name="job"
+                value={this.state.job}
+                onChange={this.handleChange}
+                required
+              />
+            </label>
+          </div>
+
+          <div>
+            <label>
+              Data de nascimento:
+              <input
+                type="date"
+                name="birth"
+                value={this.state.birth}
+                onChange={this.handleChange}
+                required
+              />
+            </label>
+          </div>
+
+          <div>
+            <label>
+              Senha:
+              <input
+                type="password"
+                name="password"
+                value={this.state.password}
+                onChange={this.handleChange}
+                required
+              />
+            </label>
+          </div>
+          <button type="submit">Enviar</button>
+        </form>
+
+       
+        {this.state.submittedData.length > 0 && (
+          <div>
+            {/* <h2>Dados Submetidos:</h2> */}
+            {this.state.submittedData.map((data, index) => (
+              <DisplayData key={index} data={data} />
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  }
+}
+
+export default Form;
